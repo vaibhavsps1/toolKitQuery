0ddddfee58551aa4c000d6924dfd99a2947fd8c2
vaibhavsps1 <120564997+vaibhavsps1@users.noreply.github.com>
2024-04-01 18:32:31 +0530

    Update drawDrag.js

--- src/screen/drawDrag.js
+++ src/screen/drawDrag.js
@@ -1,24 +1,3 @@
-// import {Text, View} from 'react-native';
-// import React from 'react';
-// import {WebView} from 'react-native-webview';
-
-// const DrawDrag = () => {
-//   return (
-//     <View style={{flex: 1, marginHorizontal: 100, justifyContent: 'center'}}>
-//       {/* <View style={{borderWidth: 2, padding: 30, width: 90}}>
-//         <Text>Hi</Text>
-//       </View> */}
-//       por
-//       <View>
-//         <WebView source={|| { uri: null }} />
-//         <View>Hi</View>
-//       </View>
-//     </View>
-//   );
-// };
-
-// export default DrawDrag;
-
 import {StyleSheet, View} from 'react-native';
 import React, {useState} from 'react';
 import axios from 'axios';
@@ -27,7 +6,6 @@ const DrawDrag = () => {
   const [quizRes, setQuizRes] = useState(null);
   const handleFormSubmit = responseData => {
     console.log('Form submitted with data:', JSON.stringify(responseData));
-    // Handle the form submission data here
   };
 
   const [isDatePickerVisible, setDatePickerVisibility] = useState(false);
@@ -45,10 +23,7 @@ const DrawDrag = () => {
     hideDatePicker();
   };
 
-  // const apiUrl = 'https://api.typeform.com/forms/oWdXojRu/responses';
-  // const accessToken =
-  //   'tfp_4gv7kRUiQouz6LASgMJktAa6M6bSSEJ5qhhubA55gcSw_3mQ2qwo3RaM7ap';
-
+ 
   const Resp1 = () => {
     let config = {
       method: 'get',
@@ -69,28 +44,26 @@ const DrawDrag = () => {
       });
   };
 
-  //   getDataWithAuthorization(apiUrl, accessToken)
-  //     .then(responseData => {
-  //       // Do something with the response data
-  //       console.log('respomse Dara ---->', responseData);
-  //       setQuizRes(responseData);
-  //     })
-  //     .catch(error => {
-  //       // Handle errors
-  //       console.log('this is err-->', error);
-  //     });
+    getDataWithAuthorization(apiUrl, accessToken)
+      .then(responseData => {
+        setQuizRes(responseData);
+      })
+      .catch(error => {
+        // Handle errors
+        console.log(error);
+      });
 
-  //   return (
-  //     <View>
-  //       <Text>{JSON.stringify(quizRes)}</Text>
-  //     </View>
-  //   );
-  // };
+    return (
+      <View>
+        <Text>{JSON.stringify(quizRes)}</Text>
+      </View>
+    );
+  };
 
   return (
     <View>
       <Text>Hi</Text>
-      {/* <View style={{borderWidth: 2, height: 500}}>
+      <View style={{borderWidth: 2, height: 500}}>
         <TypeformEmbed
           style={{flex: 1}}
           url="https://kst5crcfuzz.typeform.com/to/oWdXojRu"
@@ -102,8 +75,8 @@ const DrawDrag = () => {
       <ScrollView style={{flex: 1, borderWidth: 2}}>
         <Text>{JSON.stringify(quizRes)}</Text>
         <Text>Hi</Text>
-      </ScrollView> */}
-      {/* <View style={{width: '50%', paddingVertical: 40, padding: 30}}>
+      </ScrollView>
+      <View style={{width: '50%', paddingVertical: 40, padding: 30}}>
         <Button title="Show Date Picker" onPress={() => showDatePicker()} />
         <DateTimePickerModal
           isVisible={isDatePickerVisible}
@@ -111,7 +84,7 @@ const DrawDrag = () => {
           onConfirm={handleConfirm}
           onCancel={hideDatePicker}
         />
-      </View> */}
+      </View>
     </View>
   );
 };
